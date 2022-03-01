cd7c70bcac0ea9350e9c3cd0c680a59f3633c7cb
Francisco Carlos Calderon <nestor_05ribero@hotmail.com>
2022-03-01 11:40:02 -0500

    Update

--- User.py
+++ User.py
@@ -6,13 +6,17 @@ class User:
 
     def hacerRetiro(self, cantidad):
         self.cash -= cantidad
+        return self
 
     def hacerDeposito(self, cantidad):
         self.cash += cantidad
+        return self
 
     def mostrarBalance(self):
         print(f"{self.nombre}, Balance: ${self.cash}")
+        return self
 
     def transfer_Cash(self,receiper,amount):
         self.cash -=amount
         receiper.cash += amount
+        return self
